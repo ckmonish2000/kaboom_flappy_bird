39c234d27231aafe42be933b0e5fbb4aa709e0ec
monish karunakaran <ckmonish2000@gmail.com>
2021-12-27 22:25:00 +0530

    looping the pipes

--- src/Flappy.js
+++ src/Flappy.js
@@ -4,14 +4,16 @@ import bird from "./sprites/birdy.png"
 import bg from "./sprites/bg.jpg"
 import pipe from "./sprites/pipe.png"
 import play from "./sprites/play.png"
+import goimg from "./sprites/go.png"
 
-let { text, go, scene, onClick, action, loadSprite, add, pos, area, body, sprite, scale, keyDown, height, width, debug, origin, rand } = kaboom()
+let { loop, text, go, scene, onClick, action, loadSprite, add, pos, area, body, sprite, scale, keyDown, height, width, debug, origin, rand } = kaboom()
 
 export default function Flappy() {
   loadSprite("bg", bg)
   loadSprite("bird", bird)
   loadSprite("pipe", pipe)
   loadSprite("play", play)
+  loadSprite("go", goimg)
 
   scene("intro", () => {
     add([sprite("bg", { width: width(), height: height() })])
@@ -47,29 +49,47 @@ export default function Flappy() {
 
 
     // pipe 
-    const Pipe_gap = 100
-    const center = height() / 2
-    const ran = rand(-50, 50)
-    add([
-      sprite("pipe", { width: 150, flipY: true }),
-      pos(width() - 200, center - Pipe_gap / 2 + ran),
-      origin("botleft"),
-      // move(LEFT, 240)
-      area(),
-      "pipe"
-    ])
 
-    add([
-      sprite("pipe", { width: 150 }),
-      pos(width() - 200, center + Pipe_gap / 2 + ran),
-      area(),
-      "pipe"
-      // move(LEFT, 240)
-    ])
+    function producepipes() {
 
-    action("pipe", pipe => {
-      pipe.move(-250, 0)
-    });
+      const Pipe_gap = 250
+      const center = height() / 2
+      const ran = rand(-50, 50)
+
+      add([
+        sprite("pipe", { width: 150, flipY: true }),
+        pos(width() - 200, center - Pipe_gap / 2 + ran),
+        origin("botleft"),
+        // move(LEFT, 240)
+        area(),
+        "pipe"
+      ])
+
+      add([
+        sprite("pipe", { width: 150 }),
+        pos(width() - 200, center + Pipe_gap / 2 + ran),
+        area(),
+        "pipe"
+        // move(LEFT, 240)
+      ])
+
+      action("pipe", pipe => {
+        pipe.move(-250, 0)
+      });
+
+    }
+
+    loop(1.5, producepipes)
+
+
+    // char.action(() => {
+    //   if (char.pos.y > height() + 40 ||
+    //     char.pos.y - 30 < 0) {
+    //     go("gameover")
+    //   }
+    // })
+
+
 
     // key down event
     keyDown("space", () => {
@@ -78,8 +98,16 @@ export default function Flappy() {
 
 
     char.collides("pipe", () => {
-      console.log("collision")
+      debug.log("collision")
+      go("gameover")
     })
+  })
+
+  scene("gameover", () => {
+    add([
+      sprite("go"),
+      pos(0, 0)
+    ])
   })
 
   go("intro")
