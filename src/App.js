cfbea9e679a041039ad50137ae53c2983790b2c0
monish karunakaran <ckmonish2000@gmail.com>
2021-12-27 19:21:56 +0530

    minor

--- src/App.js
+++ src/App.js
@@ -25,10 +25,33 @@ function App() {
     app.color(127, 200, 255),
   ])
 
+  const tree = app.add([
+    app.rect(48, 64),
+    app.pos(app.width(), app.height() - 50),
+    app.outline(),
+    app.origin("botleft"),
+    app.area(),
+    app.color(255, 180, 255),
+    app.move(app.LEFT, 240),
+  ])
+
   app.onKeyPress("space", () => {
     if (player.isGrounded()) {
       player.jump();
     }
+  })
+
+
+  app.keyDown("right", () => {
+    player.move(100, 0)
+  })
+
+  app.keyDown("up", () => {
+    player.move(0, 110)
+  })
+
+  player.collides('dangerous', () => {
+    app.destroy(player)
   })
   return <></>
 }
