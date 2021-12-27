4ed7ed7e8dabf96d1e3951381838f71ba7ce49a5
monish karunakaran <ckmonish2000@gmail.com>
2021-12-27 19:57:43 +0530

    bird jump

--- src/Flappy.js
+++ src/Flappy.js
@@ -2,13 +2,21 @@ import React from 'react'
 import kaboom from "kaboom"
 import bird from "./sprites/birdy.png"
 
-let { loadSprite, add, pos, area, body, sprite } = kaboom()
+let { loadSprite, add, pos, area, body, sprite, scale, keyDown } = kaboom()
 
 export default function Flappy() {
   loadSprite("bird", bird)
 
   const char = add([
-    sprite
+    sprite("bird", { width: 90 }),
+    pos(80, 10),
+    area(),
+    body()
   ])
+
+
+  keyDown("space", () => {
+    char.jump()
+  })
   return <></>
 }
