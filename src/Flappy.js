8495b9e7b30ee6da33793d0737ee0063d7fbf5ea
monish karunakaran <ckmonish2000@gmail.com>
2021-12-27 20:07:07 +0530

    bg image

--- src/Flappy.js
+++ src/Flappy.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import kaboom from "kaboom"
 import bird from "./sprites/birdy.png"
+import bg from "./sprites/bg.jpg"
 
-let { loadSprite, add, pos, area, body, sprite, scale, keyDown } = kaboom()
+let { loadSprite, add, pos, area, body, sprite, scale, keyDown, height, width } = kaboom()
 
 export default function Flappy() {
+  loadSprite("bg", bg)
   loadSprite("bird", bird)
+
+  const bgz = add(
+    [sprite("bg", { width: width(), height: height() })]
+  )
 
   const char = add([
     sprite("bird", { width: 90 }),
