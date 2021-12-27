6336214f032ee9b8bf6755cdc4d64b13ec52302f
monish karunakaran <ckmonish2000@gmail.com>
2021-12-27 13:16:09 +0530

    int

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import kaboom from "kaboom";
 
 function App() {
+
+  const app = kaboom()
+
+  app.add([
+    app.text("hello"),
+    app.pos(120, 80),
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div></div>
   );
 }
 
